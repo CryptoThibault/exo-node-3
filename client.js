83b6d8583aa7d0c18015d637442d8e9ad24abcf7
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2021-06-01 03:04:27 +0200

    note & commentaire

--- client.js
+++ client.js
@@ -6,12 +6,16 @@ const main = async () => {
       const result = await axios.get(`http://localhost:3333${path}`)
       console.log(result.data.result)
     }
+    // Correction: Attention les requetes qui suivent ne sont pas executées en parallèle
     await logcalcApi('/calc/add/1/2')
     await logcalcApi('/calc/sub/2/1')
     await logcalcApi('/calc/mul/2/2')
     await logcalcApi('/calc/div/2/2')
     await logcalcApi('/calc/mod/3/2')
-  } catch (e) { }
+  } catch (e) {
+    // Correction: Aucune information pour l'utilisateur si tu lances client.js
+    // et que server.js n'est pas lancé ?
+  }
 }
 
 main()
